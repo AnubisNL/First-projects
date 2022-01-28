a3071e9c0ca57f3274739d4683c8faa7cb2e58bd
AnubisNL <74260641+AnubisNL@users.noreply.github.com>
2022-01-28 13:15:46 -0300

    Update index.js

--- 004/index.js
+++ 004/index.js
@@ -1,20 +1,19 @@
 function converter(){
     let bn =  document.getElementById('bn')
     let bin =  String(bn.value)
+    
+    let des = parseInt(bin, 2)
+    out.innerHTML = 'testando '+ des
+
 
     for (let i = 0; i <bin.length;i++){
-    
-    if (bin[i]!= '1' || bin[i]!= '0') {
+  
+    if (bin[i]!= '1' && bin[i]!= '0') {
         window.alert('isso nao é um número binário')
         
     }
     
 
     }
-
-
-
-    let des = parseInt(bin, 2)
-    out.innerHTML = 'testando '+ des
-
+ 
 }
